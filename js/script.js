aaa43e4f11a3efc68a7e13c2104a0cf9a189deae
Kozaeva Nastya <sacret.nk@mail.ru>
2013-01-12 22:19:49 +0400

    Styles for pager widget

--- js/script.js
+++ js/script.js
@@ -11,10 +11,19 @@
 // - http://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
-  $().ready(function(){
+  $().ready(function() {
+
     $('.views-slideshow-pager-field-item').first().before('<div class="before-widget"></div>');
     $('.views-slideshow-pager-field-item').last().after('<div class="after-widget"></div>');
-    
+    $('.views-slideshow-pager-field-item').wrapAll('<div class="slider-container"><div class="slider-scroll"></div></div>');
+    var count = $('.view-id-Gallery .views-slideshow-pager-fields .views-field-field-main-photo img').length;
+    if (count > 7) {
+      $('.view-id-Gallery .views-slideshow-controls-bottom .slider-container .slider-scroll').width(2000);
+      $('.view-id-Gallery .views-slideshow-controls-text').css('top', 0);
+      $('.view-id-Gallery .views-slideshow-controls-text').css('background', '#f8f8f5');
+    }
+    console.log(count);
+
     // prev
     $('.before-widget').click(function() {
       Drupal.viewsSlideshow.action({ "action": 'previousSlide', "slideshowID": 'main_gallery_block-page' });
@@ -28,4 +37,5 @@
     });
 
   });
+
 })(jQuery, Drupal, this, this.document);
